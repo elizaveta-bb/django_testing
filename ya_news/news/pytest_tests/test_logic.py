f03490ff0cf075be86383738975729cca51b7ada
Elizaveta Babenkova <liz2405@mail.ru>
2025-04-14 02:13:30 +0300

    ndsskdм

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -53,7 +53,7 @@ def test_author_can_delete_comment(
     response = author_client.delete(delete_comment_url)
     assertRedirects(response, f'{detail_url}#comments')
     assert not Comment.objects.filter(id=comment.id).exists(),
-    "Comment was not deleted as expected"
+        "Comment was not deleted as expected"
     
  def test_user_cant_delete_another_comment(
     reader_client,
@@ -63,7 +63,7 @@ def test_author_can_delete_comment(
     response = reader_client.delete(delete_comment_url)
     assert response.status_code == HTTPStatus.FORBIDDEN
     assert Comment.objects.filter(id=comment.id).exists(),
-    "Comment was unexpectedly deleted"
+        "Comment was unexpectedly deleted"
         
 def test_author_can_edit_comment(
     author_client,
